e97b1160c4a68cb814e7224276c10dcffd4097d5
nick neessen <nneessen@gmail.com>
2023-07-05 18:14:54 -0500

    A

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -7,6 +7,27 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const header = document.querySelector('.header');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+
+// COOKIE MSG APPEARS HERE
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.textContent = 'We use cookies for improved functionality and performance.'
+message.innerHTML = `
+    We use cookes for improved functionality and performance.
+    <button class="btn cookie--close--btn">Ok</button>"
+    `
+header.append(message)
+
+// DELETE COOKIE MSG ON CLICK
+document.querySelector('.cookie--close--btn').addEventListener('click',
+  function(){
+    message.parentElement.removeChild(message);
+  }
+)
 
 const openModal = function (e) {
   e.preventDefault();
@@ -30,3 +51,74 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+
+// ++++++++++++++++++++++++++++++ PAGE NAVIGATION
+
+// 1. ADD EVENT LISTENER TO COMMON PARENT ELEMENT
+// 2. DETERMINE WHAT EL ORIGINATED THE EVENT
+document.querySelector('.nav__links').addEventListener(
+  'click', function(e){
+    e.preventDefault();
+    // matching stratgey
+    if(e.target.classList.contains('nav__link')){
+      const id = e.target.getAttribute('href');
+      document.querySelector(id).scrollIntoView({ behavior: 'smooth'})
+    }
+  }
+)
+
+// BUTTON SCROLLING
+btnScrollTo.addEventListener('click', function(e){
+  section1.scrollIntoView({ behavior: 'smooth' })
+});
+
+
+// TABBED COMPONENET
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container')
+const tabsContent = document.querySelectorAll('.operations__content')
+
+tabsContainer.addEventListener('click', function(e){
+  e.preventDefault();
+  const clicked = e.target.closest('.operations__tab');
+
+  // GUARD CLAUSE
+  if(!clicked) return;
+
+  // remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'))
+
+  // active tab 
+  clicked.classList.add('operations__tab--active');
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active')
+
+})
+
+
+/////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+
+// const h1 = document.querySelector('h1');
+
+// // going down: child elements
+// console.log(h1.querySelectorAll('.highlight'));
+// console.log(h1.childNodes);
+// console.log(h1.children);
+// h1.firstElementChild.style.color = 'white'
+// h1.lastElementChild.style.color = 'green'
+
+// // going upwards: parents
+// console.log(h1.parentNode);
+// console.log(h1.parentElement);
+
+// h1.closest('.header').style.background = 'var(--gradient-secondary)'
+// h1.closest('h1').style.background = 'var(--gradient-primary)'
+
+// // going sideways: siblings
+// console.log(h1.previousElementSibling);
+// console.log(h1.nextElementSibling);
+// console.log(h1.previousSibling);
+// console.log(h1.nextSibling);
